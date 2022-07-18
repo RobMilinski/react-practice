76791d076e77407a26f1ac7cf0ad01a90192c3dc
Robert Milinski <robimilinski@gmail.com>
2022-07-18 11:59:07 +1000

    duplicate city check

--- src/App.js
+++ src/App.js
@@ -1,116 +1,78 @@
 import './App.css';
-import {useState} from 'react';
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import CityTemperature from './CityTemperature';
+import FormDemo from './FormDemo';
+import DismissibleToast from './DismissibleToast';
+import AccordionDemo from './AccordionDemo';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container'
 
-function App() {
-  const [cityList, setCityList] = useState([]);
-  const [city, setCity] = useState('');
-
-  const app_key = '75523f4c2e67ef3ab67afe0d532a2795';
-  
-  useEffect(() => {
-    const cityList = JSON.parse(localStorage.getItem('cityList'));
-    if (cityList) {
-      setCityList(cityList);
-      refreshAll(cityList);
-    }
-  }, []);
-
-  const onAddCity = () => {
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + app_key;
-
-    fetch(url)
-      .then(response => response.json())
-      .then(result => {
-        if (result.cod === 200) {
-          const newCityList = [...cityList, {city: city, temp: result.main.temp, country: result.sys.country}]
-          setCityList(newCityList);
-          localStorage.setItem('cityList', JSON.stringify(newCityList));
-          setCity('');
-        }
-      })
-      .catch(e => console.log('Error in fetch: '+e));
-  }
-  const onCityChange = (event) => {
-    setCity(event.target.value);
-  }
-  const deleteCity = (city) => {
-    const newCityList = cityList.filter((item) => item.city !== city)
-    setCityList(newCityList);
-    localStorage.setItem('cityList', JSON.stringify(newCityList));
-  }
-
-  const refreshCity = (city) => {
-    console.log('refreshCity '+city);
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + app_key;
-    fetch(url)
-      .then(response => response.json())
-      .then(result => {
-        if (result.cod === 200) {
-          const cityData = cityList.filter((item) => item.city === city)
-          cityData[0].temp = result.main.temp;
-          setCityList([...cityList]);
-          localStorage.setItem('cityList', JSON.stringify(cityList));
-          console.log('New temperature for city '+city+' is '+result.main.temp);
-        }
-      })
-      .catch(e => console.log('Error in fetch: '+e));
-  }
-  const refreshAll = (cl) => {
-    console.log('refreshAll ',cl);
-    cl.forEach((item) => {
-      refreshCity(item.city)
-    })
-  }
-
+function Home() {
   return (
-    <div className="App">
-      <h1>City Temperatures</h1>
-      <hr />
-      <InputGroup size="sm" className="mb-3">
-        <InputGroup.Text id="inputGroup-sizing-sm">City</InputGroup.Text>
-        <Form.Control
-          placeholder='Enter City Name'
-          value={city}
-          onChange={onCityChange}
-          aria-label="Small"
-          aria-describedby="inputGroup-sizing-sm"
-        />
-        <Button onClick={onAddCity} variant="primary">Get City Temperature</Button>
-      </InputGroup>
-      
-      <hr />
-      <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>City</th>
-          <th>Temperature</th>
-          <th>Country</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        { 
-            cityList.map(cityData =>      
-            (
-            <tr key={cityData.city}>
-              <td>{cityData.city}</td>
-              <td>{cityData.temp}</td>
-              <td>{cityData.country}</td>
-              <td onClick={() => deleteCity(cityData.city)}>&#10060; Delete</td>
-              <td onClick={() => refreshCity(cityData.city)}>Refresh</td>
-            </tr> 
-            ))
-        }
-      </tbody>
-    </Table>
+    <div>
+      <h2>Home</h2>
     </div>
   );
+}
+function NotFound() {
+  return (
+    <div>
+      <h2>Error: Not Found</h2>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Navbar bg="light" expand="md">
+      <Container fluid>
+        <Navbar.Brand href="#">Rob's React Practice</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/citytemp">City Temperature</Link></Nav.Link>
+            <Nav.Link><Link to="/dismissibletoast">Dismissible Toast</Link></Nav.Link>
+            <Nav.Link><Link to="/formdemo">Form Demo</Link></Nav.Link>
+            <Nav.Link><Link to="/accordiondemo">Accordion Demo</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/citytemp">
+            <CityTemperature />
+          </Route>
+          <Route path="/dismissibletoast">
+            <DismissibleToast />
+          </Route>
+          <Route path="/formdemo">
+            <FormDemo />
+          </Route>
+          <Route path="/accordiondemo">
+            <AccordionDemo />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+    </Router>
+  )
 }
 
 export default App;
